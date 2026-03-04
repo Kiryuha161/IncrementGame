337658817825400e5e3fcef1a174a32eae9c937e
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2026-03-04 21:40:57 +0300

    Добавил сущности в бд

---
+++ Incremental.Data/Domain/Achievement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incremental.Data.Domain
+{
+    /// <summary>
+    /// Достижения, которые дают бонусы к улучшениям
+    /// </summary>
+    public class Achievement
+    {
+        /// <summary>
+        /// Идентификатор достижения
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название достижения
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание достижения и условия его получения
+        /// </summary>
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// JSON с конфигурацией бонусов
+        /// Пример: 
+        /// {
+        ///   "clickPowerMultiplier": 1.1,
+        ///   "passiveIncomeMultiplier": 1.15,
+        ///   "discountBonus": 5,
+        ///   "powerBoostBonus": 10
+        /// }
+        /// </summary>
+        public string JsonContent { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство: все записи PlayerAchievement, использующие это достижение
+        /// </summary>
+        public ICollection<PlayerAchievement> PlayerAchievements { get; set; } = new List<PlayerAchievement>();
+    }
+}
---
+++ Incremental.Data/Domain/PlayerAchievement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incremental.Data.Domain
+{
+
+    /// <summary>
+    /// Связующая таблица, хранящая полученные достижения для конкретного игрока
+    /// </summary>
+    public class PlayerAchievement
+    {
+        /// <summary>
+        /// Уникальный идентификатор записи
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор игрока (внешний ключ к таблице Points)
+        /// </summary>
+        public int PointsId { get; set; }
+
+        /// <summary>
+        /// Идентификатор достижения (внешний ключ к таблице Achievement)
+        /// </summary>
+        public int AchievementId { get; set; }
+
+        /// <summary>
+        /// Дата и время получения достижения
+        /// </summary>
+        public DateTime UnlockedAt { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство для связи с таблицей Points
+        /// </summary>
+        [ForeignKey(nameof(PointsId))]
+        public Point Point { get; set; }
+
+        /// <summary>
+        /// Навигационное свойство для связи с таблицей Achievement
+        /// </summary>
+        [ForeignKey(nameof(AchievementId))]
+        public Achievement Achievement { get; set; }
+    }
+}
